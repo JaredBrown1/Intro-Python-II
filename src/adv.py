2388f09067ecb50cd36cc0156709d9df53fcd868
Jared Brown <jaredbrown1@hotmail.com>
2020-07-14 17:17:43 -0700

    player and room classes created

--- src/adv.py
+++ src/adv.py
@@ -38,7 +38,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = room["outside"]
+print(player)
 # Write a loop that:
 #
 # * Prints the current room name
--- src/room.py
+++ src/room.py
@@ -1,2 +1,23 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from player import Player
+
+
+class Room(Player):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.w_to = None
+        self.s_to = None
+
+    def player_direction(self, direction):
+        if direction == 'n':
+            return self.n_to
+        elif direction == 'e':
+            return self.e_to
+        elif direction == 's':
+            return self.s_to
+        else:
+            return None
